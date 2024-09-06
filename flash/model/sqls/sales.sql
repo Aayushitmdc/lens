199a228b0b74091991b1439c12e3d714009ab455
Aayushitmdc <aayushi.solanki@tmdc.io>
2024-09-06 20:22:27 +0530

    flash tableau comapaitble edited

--- flash/model/sqls/sales.sql
+++ flash/model/sqls/sales.sql
@@ -1,24 +1,24 @@
 SELECT
   site,
-  concat(site::varchar, '-', item_no::varchar) AS site_item_pk_sales,
+  CONCAT(CAST(site AS VARCHAR), '-', CAST(item_no AS VARCHAR)) AS site_item_pk_sales,
   sales_sk,
-  concat(site::varchar,'-',customer_no::varchar) AS customer_id,
-  customer_no::varchar as customer_no,
+  CONCAT(CAST(site AS VARCHAR), '-', CAST(customer_no AS VARCHAR)) AS customer_id,
+  CAST(customer_no AS VARCHAR) AS customer_no,
   site AS site_number,
   invoice_no,
-  invoice_dt_sk::timestamptz AS invoice_date,
-  date_trunc('month', invoice_dt_sk::timestamptz) AS posting_period,
+  CAST(invoice_dt_sk AS TIMESTAMP) AS invoice_date,
+  DATE_TRUNC('MONTH', CAST(invoice_dt_sk AS TIMESTAMP)) AS posting_period,
   cases,
   bottles,
   item_no,
   qty_dec_equ,
-  ext_net,
+  CAST(ext_net AS INTEGER) AS ext_net,
   unit_price,
-  invoice_dt_sk::timestamptz + (RANDOM() * 10)::integer * interval '1 day' AS posting_date,
+  CAST(invoice_dt_sk AS TIMESTAMP) + (RAND() * 10) * INTERVAL '1 DAY' AS posting_date,
   entry_origin,
   ext_net - ext_cost + ext_depl_allow + ext_participation + ext_guaranteed_adj - cqd_amt AS gross_profit,
   CASE WHEN entry_origin IN ('G', 'H') THEN 'proof' ELSE 'nonproof' END AS source
 FROM
-  "main".f_sales
+  main.f_sales
 WHERE
-  invoice_dt_sk::timestamptz >= '2022-01-01'
+  CAST(invoice_dt_sk AS TIMESTAMP) >= '2022-01-01'
